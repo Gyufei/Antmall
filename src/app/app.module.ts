6d79683b99578d5a98f478fe81094878ea4e6e62
afei <guo812773673@163.com>
2018-02-02 11:22:50 +0800

    add:页头页脚组件     bootstrap支持 mod:将相应css文件修改为scss语法格式

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HeadComponent } from './head/head.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeadComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule
